50a69c5769f8bb75c02463f5b3f51e00aff3bbf5
PangolinPaw <PangolinPaw@users.noreply.github.com>
2018-07-27 14:08:37 +0100

    initial upload

---
+++ test.js
@@ -0,0 +1,17 @@
+document.getElementById("test").addEventListener('click', () => {
+    console.log("Popup DOM fully loaded and parsed");
+
+    function modifyDOM() {
+        //You can play with your DOM here or check URL against your regex
+        sku = document.getElementsByClassName('editor_label_code')[0].innerHTML;
+        return sku
+    }
+
+    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+        document.getElementById('debug_result').innerHTML = results[0];
+    });
+});
